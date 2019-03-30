d1d8d8ac714a8b019f9ce082808bb98186a9abfd
abbracadabra <2062460209@qq.com>
2019-03-30 11:51:22 +0800

    2

--- learnbpe.py
+++ learnbpe.py
@@ -1,5 +1,7 @@
+import numpy as np
+
 in_fp = r'sss.txt'
-out_fp = r'xxx.txt'
+out_fp = r'xxx1.txt'
 
 wordmap = {}
 pairlist = []
@@ -7,14 +9,14 @@ pairlist = []
 with open(in_fp,encoding='utf-8') as f:
     for line in f:
         for w in line.split():
+            w += ' '
             if w not in wordmap:
-                wordmap[w+' '] = (list(w),1)
+                wordmap[w] = (list(w),1)
             else:
-                wordmap[w+' '][1] += 1
+                wordmap[w][1] += 1
 
 while True:
     pairmap = {}
-    maxnqueue = [] # a list to store most frequent 10 pair
     for word, (toks,count) in wordmap.items():
         for i in range(len(toks) - 1):
             pair = toks[i] + toks[i + 1]
@@ -23,30 +25,21 @@ while True:
             else:
                 pairmap[pair] += count
             pc = pairmap[pair]
-            for j,(_p, _) in enumerate(maxnqueue):
-                if _p==pair:
-                    del maxnqueue[j]
-                    break
-            if len(maxnqueue) == 0:
-                maxnqueue.append((pair, pc))
-            elif pc <= maxnqueue[0][1]:
-                if len(maxnqueue) < 10:
-                    maxnqueue.insert(0, (pair, pc))
-            else:
-                for z in range(len(maxnqueue)):
-                    if pc > maxnqueue[z][1] and (z + 1 >= len(maxnqueue) or pc <= maxnqueue[z + 1][1]):
-                        maxnqueue.insert(z + 1, (pair, pc))
-                        maxnqueue = maxnqueue[-10:]
-                        break
-    maxnqueue = maxnqueue[::-1]
-    if maxnqueue[0][1]<5: # break if the most frequent pair appear less than 5 times
+    ls_pair = []
+    ls_c = []
+    for pair, count in pairmap.items():
+        ls_pair.append(pair)
+        ls_c.append(count)
+    ix_ord = np.argsort(ls_c)[::-1][:10]
+    maxnqueue = [ls_pair[ix] for ix in ix_ord]
+    if ls_c[ix_ord[0]]<5: # break if the most frequent pair appear less than 5 times
         break
-    pairlist += [tup[0] for tup in maxnqueue]
+    pairlist += maxnqueue
     for word, (toks,count) in wordmap.items():
         i=0
         while i<len(toks)-1:
             pair = toks[i] + toks[i + 1]
-            for _p, _pc in maxnqueue:
+            for _p in maxnqueue:
                 if pair == _p:
                     del toks[i]
                     del toks[i]
@@ -54,7 +47,6 @@ while True:
                     i-=1
                     break
             i+=1
-
 
 with open(out_fp, "a",encoding='utf-8') as f:
     for _p in pairlist:
