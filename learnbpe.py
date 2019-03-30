d09f1021dda71815943a035435f2914a0fe1e062
abbracadabra <2062460209@qq.com>
2019-03-30 12:01:05 +0800

    3

--- learnbpe.py
+++ learnbpe.py
@@ -11,13 +11,13 @@ with open(in_fp,encoding='utf-8') as f:
         for w in line.split():
             w += ' '
             if w not in wordmap:
-                wordmap[w] = (list(w),1)
+                wordmap[w] = [list(w),1]
             else:
                 wordmap[w][1] += 1
 
 while True:
     pairmap = {}
-    for word, (toks,count) in wordmap.items():
+    for word, [toks,count] in wordmap.items():
         for i in range(len(toks) - 1):
             pair = toks[i] + toks[i + 1]
             if pair not in pairmap:
@@ -30,12 +30,12 @@ while True:
     for pair, count in pairmap.items():
         ls_pair.append(pair)
         ls_c.append(count)
-    ix_ord = np.argsort(ls_c)[::-1][:10]
+    ix_ord = np.argsort(ls_c)[::-1][:100]
     maxnqueue = [ls_pair[ix] for ix in ix_ord]
     if ls_c[ix_ord[0]]<5: # break if the most frequent pair appear less than 5 times
         break
     pairlist += maxnqueue
-    for word, (toks,count) in wordmap.items():
+    for word, [toks,count] in wordmap.items():
         i=0
         while i<len(toks)-1:
             pair = toks[i] + toks[i + 1]
